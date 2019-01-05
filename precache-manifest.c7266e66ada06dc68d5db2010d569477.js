8191a343594576e29d04a2c881a9614bf010c9cf
Mark Shan <markpentawatershan@gmail.com>
2019-01-05 13:17:29 -0500

    Updates

---
+++ precache-manifest.c7266e66ada06dc68d5db2010d569477.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d65b00ef1ec5eed4080f",
+    "url": "/mark-shan/static/js/runtime~main.d65b00ef.js"
+  },
+  {
+    "revision": "5369511f2c9d88c17b87",
+    "url": "/mark-shan/static/js/main.5369511f.chunk.js"
+  },
+  {
+    "revision": "effac879b8d494d7b743",
+    "url": "/mark-shan/static/js/1.effac879.chunk.js"
+  },
+  {
+    "revision": "5369511f2c9d88c17b87",
+    "url": "/mark-shan/static/css/main.31abec24.chunk.css"
+  },
+  {
+    "revision": "54b73015d1899ddd67d80f9eed183d17",
+    "url": "/mark-shan/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mark-shan/precache-manifest.0335963a869accd43c21457232e32a4c.js"
+  "/mark-shan/precache-manifest.c7266e66ada06dc68d5db2010d569477.js"
 );
 
 workbox.clientsClaim();
