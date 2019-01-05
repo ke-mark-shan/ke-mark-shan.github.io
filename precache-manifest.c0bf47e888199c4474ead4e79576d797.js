a5f191e4adb0ada0ff61b0cc3e3ad2928ea84bd7
k5shan <k5shan@edu.uwaterloo.ca>
2019-01-04 21:35:33 -0500

    Updates

---
+++ precache-manifest.c0bf47e888199c4474ead4e79576d797.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mark-shan/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d65b00ef1ec5eed4080f",
+    "url": "/mark-shan/static/js/runtime~main.d65b00ef.js"
+  },
+  {
+    "revision": "dd621469ed44f44b2a2d",
+    "url": "/mark-shan/static/js/main.dd621469.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/mark-shan/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "dd621469ed44f44b2a2d",
+    "url": "/mark-shan/static/css/main.c856c0d6.chunk.css"
+  },
+  {
+    "revision": "08dcf6bbc11c1ff4b33231b596e45f3d",
+    "url": "/mark-shan/index.html"
+  }
+];
